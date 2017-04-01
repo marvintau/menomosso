f05c903faaa7ba578207b08f6a46c5851a8b5628
Yue Tao <marvin@everstream.cn>
2017-04-01 10:43:32 +0800

    minor change

--- dungeon_base/src/dungeon_query.erl
+++ dungeon_base/src/dungeon_query.erl
@@ -120,7 +120,7 @@ add_chest_record(Conn, PlayerUUID) ->
 
 add_player_card(Conn, {CardUUID, PlayerUUID}) ->
     Query = list_to_binary(["insert into player_card_info values
-    (uuid_generate_v4(), ", CardUUID, ", '", PlayerUUID, "', now(), now());"]),
+    (uuid_generate_v4(), '", CardUUID, "', '", PlayerUUID, "', now(), now());"]),
 
     case epgsql:squery(Conn, binary_to_list(Query)) of
         {ok, 1} -> {ok, new_card_added};
