148c3c7fcb8ce5690059f91d157acb04202daa85
Yue Tao <nightvista@gmail.com>
2017-04-14 12:43:21 +0800

    single_attack

--- dungeon_base/src/battle/skills.erl
+++ dungeon_base/src/battle/skills.erl
@@ -70,7 +70,7 @@ create_skills() ->
 
     Skills = [
         {single_attack, [{0, [
-            {seq(), [test_attr(armor, def), test_attr(agility, def), test_attr(hit, def), test_attr(block, def), test_attr(dodge, def), test_attr(resist, def), test_attr(critical, def), die()]}
+            {seq(), [plain_attack()]}
         ]}]},
 
         {double_attack, [{0, [
