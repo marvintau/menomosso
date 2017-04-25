7da666b38c2dfe8efd256b5a7c76c4a2a7812731
Yue Tao <marvin@everstream.cn>
2017-04-25 16:36:05 +0800

    new_repose

--- dungeon_base/test/test_local.sh
+++ dungeon_base/test/test_local.sh
@@ -5,5 +5,5 @@ do
     #     "skills":["rune_of_the_void", "double_attack", "triple_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack"]}' http://localhost:1337/api/battle_request
     curl -H "Content-Type: application/json" -X POST\
         -d '{"id1":"8673cc53-e2a8-4375-b6a3-007e2ebe6d5f", "id2":"68b19bbe-bc2a-400f-b4e7-6e632b3b908f", "self_card_id" : "1b0cf5e0-2164-46fd-8424-2146fca99fb9",
-        "skills":["healing_potion", "poison_gas", "triple_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack"]}' http://localhost:1337/api/battle_request
+        "skills":["healing_potion", "counterattack", "triple_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack"]}' http://localhost:1337/api/battle_request
 done
