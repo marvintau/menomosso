0ca22a5f7e16f48dcbcc38c948b7644ea292782f
Yue Tao <marvin@everstream.cn>
2017-04-26 14:41:32 +0800

    new_repose

--- dungeon_base/dungeon_base.sql
+++ dungeon_base/dungeon_base.sql
@@ -94,7 +94,7 @@ insert into card_skills(skill_name, skill_multiple_time, skill_cost, card_id) va
 ('double_attack',                  true,         5,      '00000000-0000-0000-0000-000000000000'),
 ('triple_attack',                  true,         9,      '00000000-0000-0000-0000-000000000000'),
 ('charm_of_foresight',             false,        7,      '00000000-0000-0000-0000-000000000000'),
-('fortify_armor',                  false,        3,      '00000000-0000-0000-0000-000000000000'),
+('increase_dodge',                 false,        3,      '00000000-0000-0000-0000-000000000000'),
 ('increase_crit',                  false,        3,      '00000000-0000-0000-0000-000000000000'),
 ('counterattack',                  false,        5,      '00000000-0000-0000-0000-000000000000'),
 ('healing_potion',                 false,        3,      '00000000-0000-0000-0000-000000000000'),
--- dungeon_base/test/test_remote.sh
+++ dungeon_base/test/test_remote.sh
@@ -13,5 +13,5 @@ do
     #  printf "\nopen chest \n"
     curl -H "Content-Type: application/json" -X POST\
         -d '{"id1":"8673cc53-e2a8-4375-b6a3-007e2ebe6d5f", "id2":"68b19bbe-bc2a-400f-b4e7-6e632b3b908f", "self_card_id" : "1b0cf5e0-2164-46fd-8424-2146fca99fb9",
-        "skills":["fortify_armor", "double_attack", "triple_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack"]}' http://everstream.cn:1337/api/battle_request
+        "skills":["increase_dodge", "double_attack", "triple_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack", "single_attack"]}' http://everstream.cn:1337/api/battle_request
 done
