808776c29e7a2b4e9de4cb74b53ec0e1ce97dd6c
Yue Tao <marvin@everstream.cn>
2017-06-09 14:52:49 +0800

    hotfix

--- dungeon_base/src/database/sql/dungeon_query_get_player_info.erl
+++ dungeon_base/src/database/sql/dungeon_query_get_player_info.erl
@@ -18,7 +18,7 @@ get_cards_of_player(Conn, PlayerUUID) ->
 
     QueryCard = list_to_binary(["
     
-        select cards.*, frags, tem.card_level, card_stars, frags_required, coins_required from (
+        select cards.*, frags, player_card_info.card_level, card_stars, frags_required, coins_required from (
             select * from player_card_info, card_level_up
             where player_id = '", PlayerUUID, "' and player_card_info.card_level=card_level_up.card_level
         ) tem
