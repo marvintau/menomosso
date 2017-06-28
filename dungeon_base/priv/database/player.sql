82bd63aacf5469c6745082d235241b67b2d458c8
Yue Tao <nightvista@gmail.com>
2017-06-28 20:37:00 +0800

    player id changed uuid from device id

--- dungeon_base/priv/database/player.sql
+++ dungeon_base/priv/database/player.sql
@@ -27,7 +27,7 @@ create table player (
 
 insert into player
 ( player_id,                             player_name,         preset_card_id                        ) values
-('8673cc53-e2a8-4375-b6a3-007e2ebe6d5f', 'Max Planc',         '946ae77c-183b-4538-b439-ac9036024676'),
-('68b19bbe-bc2a-400f-b4e7-6e632b3b908f', 'Erwin Schodinger',  '1b0cf5e0-2164-46fd-8424-2146fca99fb9');
+("8673cc53-e2a8-4375-b6a3-007e2ebe6d5f", 'Max Planc',         '946ae77c-183b-4538-b439-ac9036024676'),
+("68b19bbe-bc2a-400f-b4e7-6e632b3b908f", 'Erwin Schodinger',  '1b0cf5e0-2164-46fd-8424-2146fca99fb9');
 
 select * from player;
